006d52178c5729ae3817e98849768ae4cf039063
deepan-crypto <deepang467@gmail.com>
2025-12-13 14:29:19 +0530

    Read

--- Index.js
+++ Index.js
@@ -1,28 +1,16 @@
-// fs: FileSystem
+// //fs: fileSystem
+// const fs = require("fs");
 
-const fs = require("fs");
+// ///blocking Code
+// // 1. path // Encryption
+// let read = fs.readFileSync("./Files/read.txt", "utf-8");
+// console.log(read);
+// console.log("Hello after async code");
 
-// blocking code
-// Synchronous code
-// 1.path, 2.encryption
-let read = fs.readFileSync("./Files/append.txt", "utf-8");
-console.log(read);
-
-// 2. Write a file
-// 1.path 2.Content 3.encryption
-fs.writeFileSync(
-  "./Files/append.txt",
-  "The file is not here but I want to write",
-  "utf-8"
-);
-
-// non-blocking code
-// asynchronous code
-// read data
-fs.readFile("./Files/read.txt", "utf-8", (err, data) => {
-  if (err) return console.log("Error comes");
-  console.log(data);
-});
-
-read = fs.readFileSync("./Files/append.txt", "utf-8");
-console.log(read);
+// // 2. Write a file
+// //path content // Encryption
+// fs.writeFileSync(
+//   "./Files/write.txt",
+//   "THe file is not here but we are writing it",
+//   "utf-8"
+// );
