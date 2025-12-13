fe4989e80603f440682b92fa8a4a9de399f181c8
deepan-crypto <deepang467@gmail.com>
2025-12-13 15:27:41 +0530

    server

--- index.js
+++ index.js
@@ -78,6 +78,9 @@ const http = require("http");
 // });
 
 
+const http = require("http");
+const fs = require("fs");
+
 const server = http.createServer((req, res) => {
 
     if (req.url === "/") {
