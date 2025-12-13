65202308a28e4dea4e9743b3980e58d197c5cadb
deepan-crypto <deepang467@gmail.com>
2025-12-13 14:29:41 +0530

    Write

--- Index.js
+++ Index.js
@@ -14,3 +14,29 @@
 //   "THe file is not here but we are writing it",
 //   "utf-8"
 // );
+
+// // non-blocking Code
+// //Async Code
+// //read data
+// // path // Encryption // callback function(error, data)
+// fs.readFile("./Files/read-this.txt", "utf-8", (error, data) => {
+//   console.log(data);
+//   fs.readFile(./Files/${data}, "utf-8", (error, data1) => {
+//     console.log(data1);
+//   });
+// });
+// console.log("Hello after async code");
+
+// //write data
+
+// //path,data,Encryption, callback function(error)
+
+// fs.writeFile(
+//   "./Files/write-this.txt",
+//   "This is async file write",
+//   "utf-8",
+//   (error) => {
+//     console.log("File is written");
+//   }
+// );
+// console.log("Hello after async code");
