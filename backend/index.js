24b46c8d5c41d5213333e282cd6009f1919678d0
deepan-crypto <deepang467@gmail.com>
2025-12-15 10:00:10 +0530

    express

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,40 @@
-const express = require("express");
-const fs = require("fs");
+const express=require("express");
+const app=express();
+const fs=require("fs");
 
-const app = express();
-
-// CRUD Operations
-// C: Creation POST
-// R: Read GET
-// U: Update Put & PATCH
-// D: Delete DELETE
-
-const PORT_NO = 9000;
-app.listen(PORT_NO, () => {
-  console.log("Server Started at ", PORT_NO);
-});
-
-app.get("/api/v1/entry", (req, res) => {
-  res.status(200).json({
-    status: "Successful",
-    length: jsonData.length,
-    data: jsonData,
-  });
-});
-
-app.get("/api/v1/entry':id", (req, res) => {
-    let id=req.params.id;
-    let plant = jsonData.find((el)=> el.id==id);
-
-    res.status(200).json
-});
-
-app.post("/api/v1/entry", (req, res) => {});
-
-app.put("/api/v1/entry/:id", (req, res) => {});
-
-app.delete("/api/v1/entry/:od", (req, res) => {});
-
-const jsonData = JSON.parse(fs.readFileSync("./plant.json", "utf-8"));
+const jsonData=JSON.parse(fs.readFileSync("./plant.json","utf-8"));
 console.log(jsonData);
+
+app.get("/api/v1/entry",(req, res)=>{
+    res.status(200).json({
+        status:"Successful",
+        length : jsonData.length,
+        data : {
+            jsonData,
+        }
+    });
+});
+app.get("/api/v1/entry/:id",(req,res)=>{
+   let id=req.params.id;
+   let entry=jsonData.find((el)=>el.id == id);
+   if(!gym){
+    res.status(404).json({
+        status:"Fail",
+        message:"Please check your entry id",
+    })
+   }
+   res.status(200).json({
+    status:"Successful",
+    gym,
+   })
+});
+//crud operation
+//c=creation post
+//r=read get
+//u=update put=>update single attribute and patch=>update everything in database
+//d=delete 
+
+const PORT_NO=9000;
+app.listen(PORT_NO,()=>{
+    console.log("Server started successfully",PORT_NO);
+})
