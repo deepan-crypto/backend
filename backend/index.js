4982e18771bc14fd010464081b592b4624240356
deepan-crypto <deepang467@gmail.com>
2025-12-15 09:45:31 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,39 @@
-const express=requie("express");
-const app =express();
+const express = require("express");
+const fs = require("fs");
 
-const jsonData=Json.parse( fs.readFileSync("./plant.json","utf-8"));
-console.log(jsonData);
+const app = express();
 
- app.get("/app/v1/")
-app.listen(9000,()=>{
-    console.log("Server Started Succesfully");
+// CRUD Operations
+// C: Creation POST
+// R: Read GET
+// U: Update Put & PATCH
+// D: Delete DELETE
+
+const PORT_NO = 9000;
+app.listen(PORT_NO, () => {
+  console.log("Server Started at ", PORT_NO);
 });
 
-// //CRUD
-// C: create
-// R: read
-// U: update
-// D: delete
+app.get("/api/v1/entry", (req, res) => {
+  res.status(200).json({
+    status: "Successful",
+    length: jsonData.length,
+    data: jsonData,
+  });
+});
 
+app.get("/api/v1/entry':id", (req, res) => {
+    let id=req.params.id;
+    let plant = jsonData.find((el)=> el.id==id);
+
+    res.status(200).json
+});
+
+app.post("/api/v1/entry", (req, res) => {});
+
+app.put("/api/v1/entry/:id", (req, res) => {});
+
+app.delete("/api/v1/entry/:od", (req, res) => {});
+
+const jsonData = JSON.parse(fs.readFileSync("./plant.json", "utf-8"));
+console.log(jsonData);
